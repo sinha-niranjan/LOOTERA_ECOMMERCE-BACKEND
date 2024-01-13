d8bbae7f93d6714fb70ca92d68ed8b8efab63995
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-01-13 18:42:15 +0530

    all folders set up Changes to be committed: 	new file:   dist/app.js 	modified:   package.json 	modified:   src/app.ts 	modified:   tsconfig.json

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,9 @@
 import express from "express";
-
 const app = express();
-
 const port = 4000;
-
-
 app.get("/product", (req, res) => {
-    res.send("dfjsdfjs")
-})
-
+    res.send("dfjsdfjs");
+});
 app.listen(port, () => {
-   
-  console.log(`Express is working on http://localhost:${port}`);
+    console.log(`Express is working on http://localhost:${port}`);
 });
