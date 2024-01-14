1a932726d11111fbfc4554a0325245654ef4a12a
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-01-15 02:25:58 +0530

    authentication is added to create a new product route Changes to be committed: 	modified:   dist/routes/product.js 	modified:   src/routes/product.ts

--- dist/routes/product.js
+++ dist/routes/product.js
@@ -2,6 +2,7 @@ import { singleUpload } from "./../middlewares/multer.js";
 import express from "express";
 // CONTROLLERS ----------------------------------------------------------------------------------------------
 import { newProduct } from "../controllers/product.js";
+import { adminOnly } from "../middlewares/auth.js";
 const app = express.Router();
-app.post("/new", singleUpload, newProduct);
+app.post("/new", adminOnly, singleUpload, newProduct);
 export default app;
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,9 +4,10 @@ import express from "express";
 // CONTROLLERS ----------------------------------------------------------------------------------------------
 
 import { newProduct } from "../controllers/product.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
-app.post("/new", singleUpload, newProduct);
+app.post("/new", adminOnly, singleUpload, newProduct);
 
 export default app;
