ef597e9301f211f96b4ce6be5eceab2e41bccb4a
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-01-24 23:52:51 +0530

    Changes to be committed: 	new file:   dist/models/coupon.js 	new file:   src/models/coupon.ts

---
+++ dist/models/coupon.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema({
+    coupon: {
+        type: String,
+        required: [true, "Please enter the Coupon Code"],
+        unique: true,
+    },
+    amount: {
+        type: Number,
+        required: [true, "Please enter the Discount Amount  "],
+    },
+});
+export const Coupon = mongoose.model("Coupon", schema);
